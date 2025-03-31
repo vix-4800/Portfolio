0f916191bf2567688c02d5eb662947447331d8f9
Anton Vix <Gordan.ei@protonmail.ch>
2025-03-31 23:21:24 +0400

    icons added

--- index.php
+++ index.php
@@ -23,6 +23,11 @@ $profession = 'Full-stack developer';
 		<?= $title; ?>
 	</title>
 
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+
 	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
 	<style type="text/tailwindcss">
